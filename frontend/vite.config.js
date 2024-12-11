696e39fa2574e54cdb7240d14e09be808bfba5ba
naseeb0007 <naserbkhan99@gmail.com>
2024-12-12 02:12:27 +0530

    Fix build directory issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Define the output directory for the build
+    outDir: 'dist', // Ensure this is set to 'dist'
+  },
+});
